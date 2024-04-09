c84e9bc17930ef425ace061f5630a50d1dc3d17d
Ishrak Adit <ishrak.adit07@gmail.com>
2024-04-10 03:06:33 +0600

    Basic Login and Register FE

--- client/src/pages/users/Login.jsx
+++ client/src/pages/users/Login.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Alert from '../../components/Alert';
 
 const Login = () => {
+
+  //Error state
+  const [error, setError] = useState(null);
+
+  //Form data states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //Handle Log in
+  const handleLogin = (e) =>{
+    e.preventDefault();
+  }
+
+
   return (
     <section className="card">
 
         <h1 className="title">Login to your account</h1>
 
         <form className='justify-center items-center h-screenitems-center'>
-            <input type="email" placeholder="Email address" className="input" autoFocus />
-            <input type="password" placeholder="Password" className="input" />
 
-            <button className="btn">Login</button>
+            <input type="email" 
+                   placeholder="Email address" 
+                   className="input" 
+                   autoFocus 
+                   value={email} 
+                   onChange={(e)=>{setEmail(e.target.value)}} />
+
+            <input type="password" 
+                   placeholder="Password" 
+                   className="input" 
+                   value={password}
+                   onChange={(e)=>setPassword(e.target.value)} />
+
+            <button className="btn" onSubmit={(e)=>{handleLogin(e)}}>Login</button>
         </form>
+
+        {error && <Alert msg={error}/>}
 
     </section>
   );
--- client/src/pages/users/Register.jsx
+++ client/src/pages/users/Register.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Alert from '../../components/Alert';
 
 const Register = () => {
+  //Error state
+  const [error, setError] = useState(null);
+
+  //Form data states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //Handle Log in
+  const handleRegister = (e) =>{
+    e.preventDefault();
+  }
+
+
   return (
-    <div>
-      From Register
-    </div>
+    <section className="card">
+
+        <h1 className="title">Create new account</h1>
+
+        <form className='justify-center items-center h-screenitems-center'>
+
+            <input type="email" 
+                   placeholder="Email address" 
+                   className="input" 
+                   autoFocus 
+                   value={email} 
+                   onChange={(e)=>{setEmail(e.target.value)}} />
+
+            <input type="password" 
+                   placeholder="Password" 
+                   className="input" 
+                   value={password}
+                   onChange={(e)=>setPassword(e.target.value)} />
+
+            <button className="btn" onSubmit={(e)=>{handleRegister(e)}}>Login</button>
+        </form>
+
+        {error && <Alert msg={error}/>}
+
+    </section>
   );
 }
 
