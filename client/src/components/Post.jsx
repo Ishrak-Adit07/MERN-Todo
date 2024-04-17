ce0e0962d9855967548fd4f7e0e5d73e1642c3b3
Ishrak Adit <ishrak.adit07@gmail.com>
2024-04-17 20:33:28 +0600

    Post flex commit

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const Post = ({post}) => {
+const Post = ({post, children}) => {
   return (
     <div className='mb-6'>
 
-        <div>
+        <div className='flex items-start justify-between'>
             <div>
                 <h2 className='font-bold text-lg text-indigo-600 first-letter:uppercase'>{post.name}</h2>
-                <p className='text-[10px] text-slate-500'>{new Date(post.createdAt).toLocaleDateString()}</p>
+                <p className='text-[10px] text-slate-500 mb-1'>{new Date(post.createdAt).toLocaleDateString()}</p>
             </div>
 
-            <div>ok</div>
+            <div>{children}</div>
         </div>
 
         <p className='text-sm'>{post.caption}</p>
 
-        <div className='h-px w-full bg-gradient-to-r from-indigo-50 via-indigo-500 to-indigo-50 mt-6'></div>
+        <div className='h-px w-full bg-gradient-to-r from-indigo-50 via-indigo-500/70 to-indigo-50 mt-6'></div>
     </div>
   );
 }
