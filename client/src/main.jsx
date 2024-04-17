34c3dcc59a5d521562bd987e6ef832b0b006ecc6
Ishrak Adit <ishrak.adit07@gmail.com>
2024-04-17 20:02:33 +0600

    Assist commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/app.css'
 import UserProvider from './Context/UserContext.jsx'
+import PostProvider from './Context/PostContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
-    <UserProvider>
-      <App />
-    </UserProvider>
+
+    <PostProvider>
+      
+      <UserProvider>
+        <App />
+      </UserProvider>
+
+    </PostProvider>
 
   </React.StrictMode>,
 )
