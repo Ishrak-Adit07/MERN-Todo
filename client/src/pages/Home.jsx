28b173e39f8702d5a07e839460a0c510a22bbae8
Ishrak Adit <ishrak.adit07@gmail.com>
2024-04-18 23:04:43 +0600

    Nav bar commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { UserContext } from '../Context/UserContext'
 
 const Home = () => {
+
+    const {user, setUser} = useContext(UserContext);
+    console.log(user);
+
+    const navigate = useNavigate();
+
+    const handleLogOut = () =>{
+        
+        if(confirm("Are you sure you want to log out?")){
+
+            setUser({
+                email:null, posts:[]
+            });
+    
+            localStorage.removeItem('email');
+            localStorage.removeItem('webToken');
+    
+            navigate("/");
+
+        }
+    }
+
   return (
     <>
         <header className="bg-indigo-500 text-white">
@@ -9,10 +32,18 @@ const Home = () => {
             <nav className="flex items-center justify-between p-4">
                 <Link title="Home" to="/" className="fa-solid fa-house-chimney nav-link"></Link>
 
-                <div className="flex items-center gap-2">
-                    <Link title="Log in" to="/login" className="fa-solid fa-right-to-bracket nav-link"></Link>
-                    <Link title="Register" to="/register" className="fa-solid fa-registered nav-link"></Link>
-                </div>
+                {user.email ? (
+                    <div className="flex items-center gap-2">
+                        <Link title="Create post" to="/createPost" className="fa-solid fa-circle-plus nav-link"></Link>
+                        <Link title="Dashboard" to="/dashboard" className="fa-solid fa-user nav-link"></Link>
+                        <button title='Log out' onClick={handleLogOut} className='fa-solid fa-right-from-bracket nav-link'></button>
+                    </div>
+                ) : (
+                    <div className="flex items-center gap-2">
+                        <Link title="Log in" to="/login" className="fa-solid fa-right-to-bracket nav-link"></Link>
+                        <Link title="Register" to="/register" className="fa-solid fa-registered nav-link"></Link>
+                    </div>
+                )}
 
             </nav>
 
