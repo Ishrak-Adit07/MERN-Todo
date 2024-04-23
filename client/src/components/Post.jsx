a3a9f45a0324afd108a4d7e7687a80175f0f3ae0
Ishrak Adit <ishrak.adit07@gmail.com>
2024-04-23 11:29:50 +0600

    DB post save commit

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -6,14 +6,14 @@ const Post = ({post, children}) => {
 
         <div className='flex items-start justify-between'>
             <div>
-                <h2 className='font-bold text-lg text-indigo-600 first-letter:uppercase'>{post.name}</h2>
+                <h2 className='font-bold text-lg text-indigo-600 first-letter:uppercase'>{post.caption}</h2>
                 <p className='text-[10px] text-slate-500 mb-1'>{new Date(post.createdAt).toLocaleDateString()}</p>
             </div>
 
             <div>{children}</div>
         </div>
 
-        <p className='text-sm'>{post.caption}</p>
+        <p className='text-sm'>{post.body}</p>
 
         <div className='h-px w-full bg-gradient-to-r from-indigo-50 via-indigo-500/70 to-indigo-50 mt-6'></div>
     </div>
